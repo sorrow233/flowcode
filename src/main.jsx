458ba0b2ef25ee3002856e40a9f3fffb1e09e381
sorrow233 <sorrow233@proton.me>
2026-01-07 02:20:34 +0900

    fix: 删除文案中的 '我们' 并升级版本至 2.4.1

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import './i18n'
+import { AuthProvider } from './contexts/AuthContext'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <App />
+        <AuthProvider>
+            <App />
+        </AuthProvider>
     </React.StrictMode>
 )
